25aa80d84eaa6ba35b114f72346da402f2c0aa8e
Michael Edwards <medwards@lyonscg.com>
2018-01-04 10:47:07 -0500

    initial commit, complete

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,22 @@
 $(function() {
+  $.ajax({
+    url: 'https://www.codeschool.com/users/melyons.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      var badgeArr = response.courses.completed;
+      addCourses(badgeArr);
+    }
+  });
 
-  // your code will go here
+  function addCourses(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        console.log(arr[i]);
+        $('#badges').append('<div class="course">' +
+          '<h3>' + arr[i].title + '</h3>' +
+          '<img src="' + arr[i].badge + '"/>' +
+          '<a href="' + arr[i].url + '" target="_blank" class="btn btn-primary">See Course</a>' +
+          '</div>');
+      }
+  }
 
 });
